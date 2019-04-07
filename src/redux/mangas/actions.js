1ea66ffcacfb9b07d975462a15d4a89b3878b495
GermanHC <ghcaunat@gmail.com>
2019-04-07 21:18:13 +0200

    Add actions Mangas

---
+++ src/redux/mangas/actions.js
@@ -0,0 +1,56 @@
+import { AsyncStorage } from "react-native";
+import _ from "lodash";
+
+import * as types from "./types";
+import * as api from "../../api";
+
+function updateMangasList(list, total) {
+  return {
+    type: types.MANGA_UPDATE_LIST,
+    list: list,
+    total
+  };
+}
+
+export function updateMangaSelected(manga) {
+  const action = {
+    type: types.MANGA_UPDATE_SELECTED,
+    value: manga
+  };
+  return action;
+}
+
+function updateFetching(value) {
+  return {
+    type: types.MANGA_UPDATE_FETCHING,
+    value
+  };
+}
+
+export function fetchMangasList() {
+  return function(dispatch, getState) {
+    dispatch(updateFetching(true));
+    AsyncStorage.getItem("MANGAS_LIST", (err, result) => {
+      let asyncStorageList = [];
+      if (result) {
+        asyncStorageList = JSON.parse(result);
+        dispatch(updateMangasList(asyncStorageList, 0));
+        dispatch(updateFetching(false));
+      }
+      api
+        .fetchMangas()
+        .then(res => {
+          const list = res.data.records;
+          const total = res.data.total;
+          if (!_.isEqual(list, asyncStorageList)) {
+            dispatch(updateMangasList(list, total));
+            AsyncStorage.setItem("MANGAS_LIST", JSON.stringify(list));
+          }
+        })
+        .catch(err => {
+          console.error("fetchMangas err: ", err);
+        })
+        .finally(() => dispatch(updateFetching(false)));
+    });
+  };
+}
